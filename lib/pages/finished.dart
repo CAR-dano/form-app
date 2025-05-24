06ea3404c6059d5888c706acbfe4739654cc6314
Dzikran Azka Sajidan <sjdn.dzikran@gmail.com>
2025-05-24 07:53:22 +0700

    feat: Refactor FinishedPage layout for improved structure and scrolling behavior

--- lib/pages/finished.dart
+++ lib/pages/finished.dart
@@ -25,59 +25,58 @@ class _FinishedPageState extends ConsumerState<FinishedPage> with AutomaticKeepA
   Widget build(BuildContext context) {
     super.build(context); // Call super.build(context) for AutomaticKeepAliveClientMixin
     return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      crossAxisAlignment: CrossAxisAlignment.center,
       children: [
         Expanded(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              SvgPicture.asset(
-                'assets/images/check.svg',
-                width: 185.5,
-                height: 185.5,
-              ),
-              const SizedBox(height: 24.0),
-              Text(
-                'Data Terkirim',
-                style: pageTitleStyle,
-                textAlign: TextAlign.center,
-              ),
-              const SizedBox(height: 8.0),
-              Text(
-                'Terima kasih atas kerja kerasnya',
-                style: labelStyle,
-                textAlign: TextAlign.center,
-              ),
-            ],
+          child: LayoutBuilder(
+            builder: (context, constraints) {
+              return SingleChildScrollView(
+                child: ConstrainedBox(
+                  constraints: BoxConstraints(
+                    minHeight: constraints.maxHeight,
+                  ),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      SvgPicture.asset(
+                        'assets/images/check.svg',
+                        width: 185.5,
+                        height: 185.5,
+                      ),
+                      const SizedBox(height: 24.0),
+                      Text(
+                        'Data Terkirim',
+                        style: pageTitleStyle,
+                        textAlign: TextAlign.center,
+                      ),
+                      const SizedBox(height: 8.0),
+                      Text(
+                        'Terima kasih atas kerja kerasnya',
+                        style: labelStyle,
+                        textAlign: TextAlign.center,
+                      ),
+                    ],
+                  ),
+                ),
+              );
+            },
           ),
         ),
         SizedBox(
           width: double.infinity,
           child: MakeNewReportButton(
-          onPressed: () {
-            ref.read(formProvider.notifier).resetFormData();
-            ref.read(imageDataListProvider.notifier).clearImageData();
-            ref.read(formStepProvider.notifier).state = 0; // Reset form step to 0
-            Navigator.of(context).pushAndRemoveUntil(
-              MaterialPageRoute(builder: (context) => const MultiStepFormScreen()), // Navigate to MultiStepFormScreen
-              (Route<dynamic> route) => false, // Remove all routes until the new one
-            );
-          },
-          text: 'Buat Laporan lagi',
+            onPressed: () {
+              ref.read(formProvider.notifier).resetFormData();
+              ref.read(imageDataListProvider.notifier).clearImageData();
+              ref.read(formStepProvider.notifier).state = 0; // Reset form step to 0
+              Navigator.of(context).pushAndRemoveUntil(
+                MaterialPageRoute(builder: (context) => const MultiStepFormScreen()), // Navigate to MultiStepFormScreen
+                (Route<dynamic> route) => false, // Remove all routes until the new one
+              );
+            },
+            text: 'Buat Laporan lagi',
+          ),
         ),
-        ),
-        // const SizedBox(height: 8.0), // Added spacing
-        // SizedBox(
-        //   width: double.infinity,
-        //   child: ElevatedButton(
-        //     onPressed: () {
-        //             ref.read(formStepProvider.notifier).state--;
-        //           },
-        //     child: const Text('Go to Page Nine'),
-        //   ),
-        // ),
         const SizedBox(height: 16.0),
         const Footer(),
       ],
