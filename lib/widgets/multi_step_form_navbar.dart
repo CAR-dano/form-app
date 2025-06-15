37745d20e914319c225bafb999a1e5be93c26a36
Dzikran Azka Sajidan <sjdn.dzikran@gmail.com>
2025-06-15 21:14:11 +0700

    fix: Adjust gradient stops and opacity for improved visual consistency in MultiStepFormNavbar

--- lib/widgets/multi_step_form_navbar.dart
+++ lib/widgets/multi_step_form_navbar.dart
@@ -35,7 +35,7 @@ class MultiStepFormNavbar extends ConsumerWidget {
                   Colors.transparent, // Top will be transparent
                   Colors.white,       // Bottom will be fully visible
                 ],
-                stops: [0.0, 0.5], // Adjust how much of the top fades out
+                stops: [0.0, 0.2], // Adjust how much of the top fades out
               ).createShader(bounds);
             },
             blendMode: BlendMode.dstIn, // Keep destination (blurred bg), mask with fade
@@ -43,7 +43,7 @@ class MultiStepFormNavbar extends ConsumerWidget {
               child: BackdropFilter(
                 filter: ImageFilter.blur(sigmaX: 50.0, sigmaY: 50.0),
                 child: Container(
-                  color: Colors.white.withOpacity(0.8), // base color of blurred layer
+                  color: Colors.white.withAlpha((0.8*255).round()), // base color of blurred layer
                 ),
               ),
             ),
@@ -52,7 +52,7 @@ class MultiStepFormNavbar extends ConsumerWidget {
 
         // Foreground content (buttons)
         Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 24.0, vertical: 24.0),
+          padding: const EdgeInsets.only(top:6, left: 24.0, right: 24.0, bottom: 24),
           child: NavigationButtonRow(
             isBackButtonEnabled: currentPageIndex > 0,
             isLastPage: currentPageIndex == formPagesLength - 2,
