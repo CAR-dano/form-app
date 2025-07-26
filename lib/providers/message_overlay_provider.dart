f0304f0cb3e9918dde5ec9a4261672d07cd888eb
Dzikran Azka Sajidan <sjdn.dzikran@gmail.com>
2025-07-26 20:53:43 +0700

    fix(message_overlay_provider): Integrate CrashlyticsUtil into CustomMessageOverlay for improved error reporting

--- lib/providers/message_overlay_provider.dart
+++ lib/providers/message_overlay_provider.dart
@@ -1,6 +1,7 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:form_app/widgets/custom_message_overlay.dart';
+import 'package:form_app/utils/crashlytics_util.dart';
 
 final customMessageOverlayProvider = Provider((ref) {
-  return CustomMessageOverlay();
+  return CustomMessageOverlay(ref.read(crashlyticsUtilProvider));
 });
