02f8dbb2ae187bd8a5568ba3b83b69bede552edb
Dzikran Azka Sajidan <sjdn.dzikran@gmail.com>
2025-08-17 20:44:58 +0700

    feat(user_info): Integrate Crashlytics for error handling in user data service

--- lib/services/user_info_service.dart
+++ lib/services/user_info_service.dart
@@ -3,9 +3,13 @@ import 'dart:io';
 import 'package:flutter/foundation.dart';
 import 'package:form_app/models/user_data.dart';
 import 'package:path_provider/path_provider.dart';
+import 'package:form_app/utils/crashlytics_util.dart'; // Import CrashlyticsUtil
 
 class UserInfoService {
   static const String _fileName = 'user_info.json';
+  final CrashlyticsUtil _crashlytics; // Add CrashlyticsUtil dependency
+
+  UserInfoService(this._crashlytics); // Constructor to receive CrashlyticsUtil
 
   Future<File> get _localFile async {
     final directory = await getApplicationDocumentsDirectory();
@@ -27,9 +31,15 @@ class UserInfoService {
           return UserData.fromJson(jsonDecode(contents));
         }
       }
-    } catch (e) {
+    } catch (e, st) { // Capture stack trace
       // Handle potential errors, e.g., file corruption
       debugPrint('Error reading user data: $e');
+      _crashlytics.recordError(
+        e,
+        st,
+        reason: 'Error reading user data from local storage',
+        fatal: false, // Not fatal, as it might just mean no user data exists yet
+      );
     }
     return null;
   }
